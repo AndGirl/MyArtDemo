2ae9975c25814bcaf065b4f11c650c1826038c48
YBJ <15390264297@163.com>
2017-12-19 19:09:31 +0800

    svg

---
+++ mesvgdemo/src/main/java/com/ybj/mesvgdemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.ybj.mesvgdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * SVG是基于XML的描述
+ * https://github.com/geftimov/android-pathview
+ * http://www.iconfont.cn
+ * http://oss.chengxingyao.cn/svg2android/index.html
+ *
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
