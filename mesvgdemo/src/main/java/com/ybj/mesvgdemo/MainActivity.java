bfd70a8c31d40f070178d6d14ca42eb9d1f8c94c
YBJ <15390264297@163.com>
2017-12-19 19:54:02 +0800

    使用第三方android-pathView（不推荐）实现颜色更改

--- mesvgdemo/src/main/java/com/ybj/mesvgdemo/MainActivity.java
+++ mesvgdemo/src/main/java/com/ybj/mesvgdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.ybj.mesvgdemo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.eftimoff.androipathview.PathView;
 
 /**
  * SVG是基于XML的描述
@@ -16,5 +19,43 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final PathView pathView = (PathView) findViewById(R.id.pathView);
+
+//        final Path path = new Path();
+//
+//        pathView.setPath(path);
+//        final Path path = makeConvexArrow(50, 100);
+//        pathView.setPath(path);
+        //       pathView.setFillAfter(true);
+        //  pathView.useNaturalColors();
+//        pathView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                pathView.getPathAnimator().
+//                        //pathView.getSequentialPathAnimator().
+//                                delay(100).
+//                        duration(1500).
+//                        interpolator(new AccelerateDecelerateInterpolator()).
+//                        start();
+//            }
+//        });
+
+        //开启图片
+        pathView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pathView.getPathAnimator()
+                        .start();
+            }
+        });
+
+        //使用svg提供的默认图片
+        //pathView.useNaturalColors();
+
+        //执行完动画是否需要保持
+        pathView.setFillAfter(true);
+
+
     }
 }
