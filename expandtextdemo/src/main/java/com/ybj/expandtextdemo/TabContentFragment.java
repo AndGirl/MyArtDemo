89504cd66cedfe838966d20495f0d5627d2581d4
YBJ <15390264297@163.com>
2018-01-24 22:56:57 +0800

    tablayout实现底部状态栏

---
+++ expandtextdemo/src/main/java/com/ybj/expandtextdemo/TabContentFragment.java
@@ -0,0 +1,25 @@
+package com.ybj.expandtextdemo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 杨阳洋 on 2018/1/24.
+ */
+
+public class TabContentFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tabcontent,null);
+    }
+
+    public static Fragment newInstance(String s) {
+        return new TabContentFragment();
+    }
+}
